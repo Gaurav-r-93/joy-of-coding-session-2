4b7baec3580ed73b0d2ce3458d7638e4f1cbdd81
Gaurav-r-93 <92088535+Gaurav-r-93@users.noreply.github.com>
2021-10-08 17:01:00 +0530

    Create 1votingternary.c

---
+++ 1votingternary.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+  int age
+    printf("enter your age\n");
+  scanf("%d", &age);
+  (age>=18)?printf("yes,you are eligible for voting!"):printf("you are not eligible for voting!");
+  return 0;
+}
